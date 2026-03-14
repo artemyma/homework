fdd70204356870df08cabfee238adf319a803f8b
artemyma <artemyma77@xmail.ru>
2026-03-14 16:09:36 +0700

    Add files via upload

---
+++ lesson-10/main.js
@@ -0,0 +1,27 @@
+// Задача 1.
+
+const firstName = "Артемий";
+const lastName = "Малютин";
+const isStudent = true;
+
+// Задача 2.
+
+const age = 26;
+const currentYear = 2026;
+const birthYear = currentYear - age;
+
+// Задача 3.
+
+const result = `Меня зовут ${firstName} ${lastName} мне ${age} лет. Я ученик курса: ${isStudent}`
+console.log(result)
+
+// Задача 4.
+
+//перевел типо что получится
+// let result_ = '123' + 456 + 789 + false + true
+
+//получаем строку
+// 12345678901falsetrue
+
+
+
