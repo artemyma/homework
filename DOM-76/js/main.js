db52980f52c40bcc2b72a74e9b65641f332db30b
artemyma <artemyma77@xmail.ru>
2026-03-30 23:51:57 +0700

    Add files via upload

--- DOM-76/js/main.js
+++ DOM-76/js/main.js
@@ -1,103 +1,11 @@
-"use strict";
+import { getTodosFromLocalStorage } from "./storage.js"
+import { renderTodos, initTodoHandlers } from "./dom.js"
 
-const todoKeys = {
-  id: "id",
-  text: "text",
-  is_completed: "is_completed",
-};
 
-const todos = [];
+const todos = getTodosFromLocalStorage() || [];
 
-const errTodoNotFound = todoId => `Todo with id ${todoId} not found`;
-
-const getNewTodoId = todos =>
-  todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
-
-const createTodo = (todos, text) => {
-  const newTodo = {
-    [todoKeys.id]: getNewTodoId(todos),
-    [todoKeys.text]: text,
-    [todoKeys.is_completed]: false,
-  };
-  todos.push(newTodo);
-  return newTodo;
-};
-
-const completeTodoById = (todos, todoId) => {
-  const todo = todos.find(todo => todo[todoKeys.id] === todoId);
-
-  if (!todo) {
-    console.error(errTodoNotFound(todoId));
-    return null;
-  }
-  todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
-  return todo;
-};
-
-const deleteTodoById = (todos, todoId) => {
-  const todoIndex = todos.findIndex(todo => todo[todoKeys.id] === todoId);
-  if (todoIndex === -1) {
-    console.error(errTodoNotFound(todoId));
-    return todos;
-  }
-  todos.splice(todoIndex, 1);
-  return todos;
-};
-
-// При помощи метода querySelector получаем элементы .form, .input и .todos
-
-const formElement = document.querySelector(".form")
-const inputElement = document.querySelector(".input")
-const todosElement = document.querySelector(".todos")
-
-// Создаем функцию createTodoElement(text), которая будет создавать todo в виде разметки
-
-const createTodoElement = todo => {
-  const todoElement = document.createElement("li")
-  todoElement.classList.add("todo")
-  todoElement.dataset.id = todo[todoKeys.id]
-  todoElement.innerHTML = `<div class="todo-text"> 
-  ${todo[todoKeys.text]}
-  </div>
-  <div class="todo-actions">
-  <button class="button-complete button">&#10004;</button>
-  <button class="button-delete button">&#10006;</button>
-  </div>`
-
-  return todoElement
-}
-
-// Создаем функцию handleCreateTodo(todos, text), которая будет вызывать createTodo и createTodoElement
-
-const handleCreateTodo = (todos, text) => {
-  const todo = createTodo(todos, text);
-  const todoElement = createTodoElement(todo);
-  todosElement.prepend(todoElement);
-  return todo;
-};
-
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault()
-  const text = inputElement.value.trim()
-  if (text == "") {
-    return
-  }
-  handleCreateTodo(todos, text)
-  inputElement.value = ""
+document.addEventListener("DOMContentLoaded", () => {
+  renderTodos(todos)
+  initTodoHandlers(todos)
 })
 
-
-todosElement.addEventListener("click", event => {
-  const todo = event.target.closest(".todo")
-  if (!todo) return
-
-  if (event.target.matches(".button-complete")) {
-    completeTodoById(todos, Number(todo.dataset.id))
-    todo.classList.toggle("completed")
-  }
-
-  if (event.target.matches(".button-delete")) {
-    deleteTodoById(todos, Number(todo.dataset.id))
-    todo.remove()
-  }
-})
